186e941fdbb217bbba36c4f1b10ca6c11722bcc3
Zhanna <kezhanna.03@mail.ru>
2025-11-18 22:25:53 +0500

    task 15

--- script.js
+++ script.js
@@ -172,5 +172,5 @@ const myFavoriteFootballTeam = {
 };
  
 Object.freeze(myFavoriteFootballTeam);
-const sport = myFavoriteFootballTeam.sport;
-const team = myFavoriteFootballTeam.team;
+
+const {sport, team} = myFavoriteFootballTeam;
